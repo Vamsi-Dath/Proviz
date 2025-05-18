a0422874862ec0d2a63e8c0c02e2a97cb458ed9d
Vamsi Dath <vamsi@Mac.attlocal.net>
2025-05-18 01:46:24 -0500

    added api fetch

--- frontend/proviz-frontend/src/App.tsx
+++ frontend/proviz-frontend/src/App.tsx
@@ -1,16 +1,36 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import BoxItem from './components/BoxItem'
 
 function App() {
   const [count, setCount] = useState(0);
+  const [data, setData] = useState<{ greeting: string }[]>([]);
   const handleClick = () => {
     setCount((count) =>
       count + 1
     )
   };
+  const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  const getData = () => {  
+    fetch(BACKEND_URL + '/api/data',{
+      method: 'GET'
+    }).then((response) => response.json())
+      .then((data) => {
+        setData(data);
+      })
+  };
 
   return (
     <>
+      <ul>
+        {data && data.map((item, index) => (
+          <li key={index}>{item.greeting}</li>
+        ))}
+      </ul>
       <BoxItem cnt={count} onBtnClick={handleClick} />
     </>
   )
