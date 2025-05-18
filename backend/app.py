45cde2696dd0233b66c6dd642d9797a221501871
Vamsi Dath <vamsi@Mac.attlocal.net>
2025-05-18 01:43:00 -0500

    added basic api

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,23 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, origins=["http://localhost:5173", "http://127.0.0.1:5173"])
+
 
 @app.route('/')
 def health():
-    return jsonify({'message': 'backend runnning OK'})
+    return jsonify({'message': 'backend running OK'})
+
+@app.route('/api/data', methods=['GET'])
+def load_data():
+    data = [{
+        'greeting': 'hello from backend'
+    },{
+        'greeting': 'successfully fetched the data'
+    }]
+    print('getting data')
+    return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="127.0.0.1", port=5001, debug=True)
