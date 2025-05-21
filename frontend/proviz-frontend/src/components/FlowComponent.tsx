b914cb24ce56182afe36f1482e99e7138c1b3a9f
Vamsi Dath <vamsi@Mac.attlocal.net>
2025-05-20 19:14:18 -0500

    explored basic events and props:

--- frontend/proviz-frontend/src/components/FlowComponent.tsx
+++ frontend/proviz-frontend/src/components/FlowComponent.tsx
@@ -1,4 +1,4 @@
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import {
   Background,
   Controls,
@@ -8,6 +8,7 @@ import {
   useNodesState,
   useEdgesState,
   type OnConnect,
+  type ColorMode
 } from "@xyflow/react";
 
 import "@xyflow/react/dist/style.css";
@@ -22,6 +23,13 @@ export default function FlowComponent() {
     (connection) => setEdges((edges) => addEdge(connection, edges)),
     [setEdges]
   );
+  const [mode, setMode] = useState<ColorMode>('dark');
+  const handleModeChangeStart = () => {
+    setMode('light');
+  };
+  const handleModeChangeEnd = () => {
+    setMode('dark');
+  };
 
   return (
     <ReactFlow
@@ -33,6 +41,11 @@ export default function FlowComponent() {
       onEdgesChange={onEdgesChange}
       onConnect={onConnect}
       fitView
+      snapToGrid
+      defaultMarkerColor='#63e3dc'
+      colorMode={mode}
+      onNodeDragStart={handleModeChangeStart}
+      onNodeDragStop={handleModeChangeEnd}
     >
       <Background />
       <MiniMap />
